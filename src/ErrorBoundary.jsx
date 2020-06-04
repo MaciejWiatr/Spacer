ce8c53b760c93bbe185c70c68b12712224b4a0a7
Dettlaff <maciekwiatr17@gmail.com>
2020-06-04 03:13:25 +0200

    Finish mvc

---
+++ src/ErrorBoundary.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+class ErrorBoundary extends React.Component {
+    state = { hasError: false };
+
+    static getDerivedStateFromError(error) {
+        return { hasError: true };
+    }
+
+    componentDidCatch(error, errorInfo) {
+        console.error(error);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <h1>Something went wrong.</h1>;
+        }
+
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
